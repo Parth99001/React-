e553a12dc77ea046ae330946943340476a017f86
Parth99001 <parthgandhi350@gmail.com>
2024-11-29 17:05:48 +0530

    Add files via upload

---
+++ Bootstrap/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ Bootstrap/src/Components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div><h1>Hello I'm Parth</h1></div>
+  )
+}
+
+export default Main
